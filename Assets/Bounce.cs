bd9d8a5df648777dead0db505b610f1828cbc14f
hafidz0098 <hafidznak123@gmail.com>
2022-10-27 03:09:59 +0700

    add movement controller and platform bounce

---
+++ Assets/Bounce.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>().velocity.y <= 0)
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * 300f);
+        }
+    }
+}
