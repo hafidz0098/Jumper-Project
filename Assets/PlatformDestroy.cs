eaf9c59dc446e790568eb6e03e4f22718d11ae64
cnovandinata <95232744+cnovandinata@users.noreply.github.com>
2022-10-27 20:27:54 +0700

    test

--- Assets/PlatformDestroy.cs
+++ Assets/PlatformDestroy.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class PlatformDestroy : MonoBehaviour
 {
     float timer;
+    bool onScreen;
     // Start is called before the first frame update
     void Start()
     {
-        
+        onScreen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if(timer > 25)
+        if(timer > 35)
         {
             Destroy(gameObject);
         }
+    }
+
+    void OnBecameVisible()
+    {
+        onScreen = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        if(onScreen)
+            Destroy(gameObject);
     }
 }
--- Assets/PlatformSpawn.cs
+++ Assets/PlatformSpawn.cs
@@ -7,56 +7,44 @@ public class PlatformSpawn : MonoBehaviour
     public GameObject platform;
     float timer;
     float height, position;
+    float speed;
+    float camHeight;
     Vector3 spawn;
 
     // Start is called before the first frame update
     void Start()
     {
+        speed = 0.0012f;
+        camHeight = -4;
         height = -4;
         position = 0;
         spawn = new Vector3(position, height, 0);
         Instantiate(platform, spawn , Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
+        height += Random.Range(1.5f, 2.5f);
         position = Random.Range(-2f,2f);
         spawn = new Vector3(position, height, 0);
         Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
+        height += Random.Range(1.5f, 2.5f);
         position = Random.Range(-2f, 2f);
         spawn = new Vector3(position, height, 0);
         Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
+        height += Random.Range(1.5f, 2.5f);
         position = Random.Range(-2f, 2f);
         spawn = new Vector3(position, height, 0);
         Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
+        height += Random.Range(1.5f, 2.5f);
         position = Random.Range(-2f, 2f);
         spawn = new Vector3(position, height, 0);
         Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
+        height += Random.Range(1.5f, 2.5f);
         position = Random.Range(-2f, 2f);
         spawn = new Vector3(position, height, 0);
         Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
+        height += Random.Range(1.5f, 2.5f);
         position = Random.Range(-2f, 2f);
         spawn = new Vector3(position, height, 0);
         Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
-        position = Random.Range(-2f, 2f);
-        spawn = new Vector3(position, height, 0);
-        Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
-        position = Random.Range(-2f, 2f);
-        spawn = new Vector3(position, height, 0);
-        Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
-        position = Random.Range(-2f, 2f);
-        spawn = new Vector3(position, height, 0);
-        Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
-        position = Random.Range(-2f, 2f);
-        spawn = new Vector3(position, height, 0);
-        Instantiate(platform, spawn, Quaternion.identity);
-        height += Random.Range(0.75f, 1.5f);
+        height += Random.Range(1.5f, 2.5f);
         position = Random.Range(-2f, 2f);
         spawn = new Vector3(position, height, 0);
         Instantiate(platform, spawn, Quaternion.identity);
@@ -66,10 +54,18 @@ public class PlatformSpawn : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        if (timer > 1)
+        if (timer >= 24)
         {
-            timer = 0;
-            height += Random.Range(0.75f, 1.5f);
+            if (speed <= 0.0024f)
+            {
+                speed += 0.0001f;
+                timer = 12;
+            }
+        }
+        camHeight += speed;
+        if (height <= camHeight + 11)
+        {
+            height += Random.Range(1.5f, 2.5f);
             position = Random.Range(-2f, 2f);
             spawn = new Vector3(position, height, 0);
             Instantiate(platform, spawn, Quaternion.identity);
